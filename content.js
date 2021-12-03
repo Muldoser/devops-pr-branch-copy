c2a165a001253e6813c040712a7f14577c754a9f
Michiel Mulder <michielmldr@gmail.com>
2021-12-03 12:38:58 +0100

    feat: add button on page reroute

--- content.js
+++ content.js
@@ -1,6 +1,3 @@
-const prHeaderBranchCollection = document.getElementsByClassName('pr-header-branches');
-console.log('prHeaderBranchCollection: ', prHeaderBranchCollection);
-
 function copyToClipboard (text) {
   navigator.clipboard.writeText(text);
 };
@@ -9,17 +6,40 @@ function actionButtonClick (text) {
   copyToClipboard(text);
 }
 
+const azureDevOpsActionButtonId = 'devops-pr-branch-copy__copy-button';
 const azureDevOpsActionButton = `
-<div class="bolt-clipboard-button hash-copy-button repos-commit-header-hash"><button aria-label="Copy to clipboard" class="bolt-button bolt-icon-button enabled subtle icon-only bolt-focus-treatment" data-focuszone="" data-is-focusable="true" role="button" tabindex="0" type="button"><span aria-hidden="true" class="left-icon flex-noshrink fabric-icon ms-Icon--Copy medium"></span></button></div>
+<div id="${azureDevOpsActionButtonId}" class="bolt-clipboard-button hash-copy-button repos-commit-header-hash"><button aria-label="Copy to clipboard" class="bolt-button bolt-icon-button enabled subtle icon-only bolt-focus-treatment" data-focuszone="" data-is-focusable="true" role="button" tabindex="0" type="button"><span aria-hidden="true" class="left-icon flex-noshrink fabric-icon ms-Icon--Copy medium"></span></button></div>
 `;
 
-if (!!prHeaderBranchCollection?.length && !!prHeaderBranchCollection[0]?.children?.length) {
-  const prHeaderBranches = prHeaderBranchCollection[0];
-  const branchAnchor = prHeaderBranches?.children[0];
-  console.log('branchAnchor: ', branchAnchor);
-  const copyText = branchAnchor?.innerHTML || '';
-  prHeaderBranches.innerHTML += azureDevOpsActionButton;
-  const copyActionButton = document.getElementsByClassName('bolt-clipboard-button');
-  copyActionButton.addEventListener('click', actionButtonClick(copyText));
-  prHeaderBranches.append(copyActionButton);
+function attemptCopyButtonRender() {
+  const prHeaderBranchCollection = document.getElementsByClassName('pr-header-branches');
+  if (!!prHeaderBranchCollection?.length && !!prHeaderBranchCollection[0]?.children?.length) {
+    const prHeaderBranches = prHeaderBranchCollection[0];
+    const branchAnchor = prHeaderBranches?.children[0];
+    const copyText = branchAnchor?.innerHTML || '';
+
+    if (!!prHeaderBranches?.innerHTML?.length) {
+      let copyActionButton = document.getElementById(azureDevOpsActionButtonId);
+      // If the button already exists, don't add a new one
+      if (!copyActionButton) {
+        prHeaderBranches.innerHTML += azureDevOpsActionButton;
+        copyActionButton = document.getElementById(azureDevOpsActionButtonId);
+        if (!!copyActionButton) {
+          prHeaderBranches.append(copyActionButton);
+
+          copyActionButton.addEventListener('click', actionButtonClick(copyText));
+        }
+      }
+    }
+  }
 }
+
+attemptCopyButtonRender();
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    // listen for messages sent from background.js
+    if (request.message === 'devops-route-change') {
+      attemptCopyButtonRender();
+    }
+});
